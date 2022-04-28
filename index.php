1b11766629c4718f348b02a114efc6bbf4071c81
Ben8544 <104559860+Ben8544@users.noreply.github.com>
2022-04-28 12:55:54 +0200

    Image MAJ

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ print "le jour est  ".$time;
 <html>
 <body>
 hello world
-  <img src="/upload/emissions/normal/616d612ce73fb9.90859299.png" class="cover-img">
+  <img src="https://digradio-nordvendee.fr/upload/emissions/normal/616d612ce73fb9.90859299.png" class="cover-img">
 </body>
 </html> 
